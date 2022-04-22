00d6c9d5cb04d01888b382b50724699265b3e4a1
tomnisim <tomnisi@post.bgu.ac.il>
2022-04-22 10:35:34 +0300

    added pair class

---
+++ src/Domain/Utils/Pair.java
@@ -0,0 +1,71 @@
+package Domain.Utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// Pair class
+public class Pair<U, V>
+{
+    private final U first;       // the first field of a pair
+    private final V second;      // the second field of a pair
+
+    // Constructs a new pair with specified values
+    private Pair(U first, V second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    // Checks specified object is "equal to" the current object or not
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        // call `equals()` method of the underlying objects
+        if (!first.equals(pair.first)) {
+            return false;
+        }
+        return second.equals(pair.second);
+    }
+
+    @Override
+    // Computes hash code for an object to support hash tables
+    public int hashCode()
+    {
+        // use hash codes of the underlying objects
+        return 31 * first.hashCode() + second.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+
+    public U getFirst()
+    {
+        return this.first;
+    }
+
+    public V getSecond()
+    {
+        return this.second;
+    }
+
+    // Factory method for creating a typed Pair immutable instance
+    public static <U, V> Pair <U, V> of(U a, V b)
+    {
+        // calls private constructor
+        return new Pair<>(a, b);
+    }
+}
