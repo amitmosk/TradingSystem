7f2041e926cb45701ce189380c5cb1c25412c48a
tomnisim <tomnisi@post.bgu.ac.il>
2022-04-22 10:43:44 +0300

    added pair class

--- src/Domain/StoreModule/Basket.java
+++ src/Domain/StoreModule/Basket.java
@@ -1,9 +1,11 @@
 package Domain.StoreModule;
 
+import Domain.Utils.Pair;
+
 import java.util.HashMap;
 
 public class Basket {
-    private Pair <Integer, Integer> basket_id; // <user_id, store_id>
+    private Pair<Integer, Integer> basket_id; // <user_id, store_id>
     private HashMap<Integer, Product> products;
     private double total_price;
 
