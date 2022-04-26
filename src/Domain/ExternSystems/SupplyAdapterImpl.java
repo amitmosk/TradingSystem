e1950c1a95d89a6e71f0eb04502f831b62515916
galbrow <63191238+galbrow@users.noreply.github.com>
2022-04-26 17:42:30 +0300

    added adapters

--- src/Domain/ExternSystems/SupplyAdapterImpl.java
+++ src/Domain/ExternSystems/SupplyAdapterImpl.java
@@ -1,10 +1,10 @@
 package Domain.ExternSystems;
 
 import Domain.ExternSystems.Proxy.ExternSupplySystemProxy;
-import Domain.ExternSystems.SupplyAdapter;
 
 public class SupplyAdapterImpl implements SupplyAdapter {
     private ExternSupplySystemProxy externSupplySystemProxy;
+
     @Override
     public boolean supply() {
         // TODO: logger
