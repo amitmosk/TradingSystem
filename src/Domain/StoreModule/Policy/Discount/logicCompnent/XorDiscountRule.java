00a8bc260e7c6bb82d0c18d5f0043920ee4bfe47
amitgrum8 <63203500+amitgrum8@users.noreply.github.com>
2022-05-15 11:11:22 +0300

    changed xor for bar asking

--- src/Domain/StoreModule/Policy/Discount/logicCompnent/XorDiscountRule.java
+++ src/Domain/StoreModule/Policy/Discount/logicCompnent/XorDiscountRule.java
@@ -8,32 +8,24 @@ import Domain.StoreModule.Policy.Predict;
 public class XorDiscountRule implements DiscountRule {
     private SimpleDiscountRule Left;
     private SimpleDiscountRule Right;
-    private boolean LeftOrRight;//left =true right=false
 
-    public XorDiscountRule(SimpleDiscountRule Left, SimpleDiscountRule Right, boolean LeftOrRight) {
+    public XorDiscountRule(SimpleDiscountRule Left, SimpleDiscountRule Right) {
         this.Left = Left;
         this.Right = Right;
-        this.LeftOrRight = LeftOrRight;
     }
 
     @Override
     public double CalculatePriceAfterDiscount(Basket basket) {
         double discountFromLeft = Left.CalculatePriceAfterDiscount(basket);
         double discountFromRight = Right.CalculatePriceAfterDiscount(basket);
-        boolean XorRes = discountFromLeft != 0 ^ discountFromRight != 0;
-        if (XorRes)
+        boolean XorRes = (discountFromLeft != 0) ^ (discountFromRight != 0);
+        if (XorRes)//if XorRes==true then only one discount is working
             return Math.max(discountFromLeft, discountFromRight);
-        else if(LeftOrRight)//left =true right =false
-            return discountFromLeft;
-        else
-            return discountFromRight;
-
+        else return Math.min(discountFromLeft, discountFromRight);
     }
 
     @Override
     public boolean CanApply(Basket basket) {
-        boolean Leftres = Left.CanApply(basket);
-        boolean Rightres = Right.CanApply(basket);
-        return Leftres ^ Rightres;
+        return true;
     }
 }
