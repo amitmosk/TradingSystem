ab72d854a31161ce7527faa43c8cbd00703a164d
tomnisim <tomnisi@post.bgu.ac.il>
2022-05-21 13:59:15 +0300

    added from dialog

--- Client/src/App.js
+++ Client/src/App.js
@@ -56,7 +56,7 @@ export default class App extends Component {
     return (
       <BrowserRouter>
       <Routes>
-      <Route path="/" element={<StoreManagment />}></Route>
+      <Route path="/" element={<Album />}></Route>
       {/* <Route path="/" element={<Login isLoggedIn={this.state.isLoggedIn} loginUpdateHandler={this.updateLoginHandler}/>}></Route> */}
       <Route path="/Register" element={<Register />}></Route>
       <Route path="/HomePageSearch" element={<HomePageSearch />}></Route>
